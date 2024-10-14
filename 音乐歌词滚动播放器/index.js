985ee197ea5802b4e4e7436de5a37d79adac5fe7
David <129467241+Durreng@users.noreply.github.com>
2024-10-14 17:35:10 +0800

    Add files via upload

--- 音乐歌词滚动播放器/index.js
+++ 音乐歌词滚动播放器/index.js
@@ -1,4 +1,4 @@
-(function () {
+(async function () {
     /**
      * 从网格获取歌词数据
      * @returns 
@@ -15,12 +15,19 @@
         container: document.querySelector('.container')
     }
 
+    const size = {
+        liHeight: 30,
+        constainerHeight: 420
+    }
+
+    let lrcData;
+
     // 1.初始化
     async function init() {
-        // 拿到歌词，生成li，放入到ul中
+        // 拿到歌词，生成 li ，放入到 ul 中
         const lrc = await getLrc();
         // 将lrc -> [{ time: 86.09, words: '歌词内容' } ...]
-        const result = lrc.split("\n").filter((s) => s).map((s) => {
+        lrcData = lrc.split("\n").filter((s) => s).map((s) => {
             const parts = s.split("]");
             const timeParts = parts[0].replace('[', '').split(':');
 
@@ -30,9 +37,37 @@
             };
         });
 
-        console.log(result);
+        // 生成 li ，加入到 ul 中
+        doms.ul.innerHTML = lrcData.map((lrc) => `<li>${lrc.words}</li>`).join('');
     }
-    init();
+    await init();
 
     // 2.交互事件
+    // 1) 什么事件 2) 如何处理
+    // audio 元素的播放进度变化的事件
+    doms.audio.addEventListener('timeupdate', function () {
+        setStatus(this.currentTime);
+    });
+
+    // 根据当前的播放进度，设置歌词状态
+    function setStatus(time) {
+        time += 0.5;
+        // 1.根据时间找到对应的 li 高亮
+        // 消除之前的 active
+        const activeLi = document.querySelector('.active');
+        activeLi && activeLi.classList.remove('active');
+        const index = lrcData.findIndex(lrc => lrc.times > time) - 1;
+
+        if (index < 0) {
+            return;
+        }
+        doms.ul.children[index].classList.add('active');
+        // 2.设置ul的滚动位置
+        let top = size.liHeight * index + size.liHeight / 2 - size.constainerHeight / 2;
+        top = -top;
+        if (top > 0) {
+            top = 0;
+        }
+        doms.ul.style.transform = `translateY(${top}px)`;
+    }
 })();
